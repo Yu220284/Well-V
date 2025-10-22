69b7e9a3ce1f1733099a697b6234bae8d4380cd7
YU IWASE <from220to284@gmail.com>
2025-10-22 06:10:08 +0000

    回転の向きを逆にして

--- src/components/session/Player.tsx
+++ src/components/session/Player.tsx
@@ -42,14 +42,14 @@ const Rewind10Icon = (props: React.SVGProps<SVGSVGElement>) => (
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M3 13a9 9 0 1 0 1.5-5.5" />
-      <path d="M4 19.5V16h3.5" />
+      <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+      <path d="M3 3v5h5" />
       <text
         x="12"
-        y="14"
+        y="12.5"
         textAnchor="middle"
         dominantBaseline="middle"
-        fontSize="12"
+        fontSize="8"
         fill="currentColor"
       >
         10
@@ -68,14 +68,14 @@ const FastForward10Icon = (props: React.SVGProps<SVGSVGElement>) => (
       strokeLinejoin="round"
       {...props}
     >
-        <path d="M21 13a9 9 0 1 1-1.5-5.5" />
-        <path d="M20 19.5V16h-3.5" />
+        <path d="M21 12a9 9 0 1 1-6.72-1.66L21 8" />
+        <path d="M21 3v5h-5" />
         <text
             x="12"
-            y="14"
+            y="12.5"
             textAnchor="middle"
             dominantBaseline="middle"
-            fontSize="12"
+            fontSize="8"
             fill="currentColor"
         >
             10
